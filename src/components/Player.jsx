2d69b8094610a7a2c17339f63421524352c89d11
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-12-31 09:47:33 +0100

    the usuall approach

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleNameChange(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleSubmitted() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={playerName} />
+        <button onClick={handleSubmitted}>Set Name</button>
       </p>
     </section>
   );
